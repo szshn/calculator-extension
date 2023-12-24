64c16bb068cef4e154dafbfdf6ef0d41924a6f94
Sujin Shin <sujins2@illinois.edu>
2023-12-24 17:17:20 +0800

    Basic test cases passed. Enabled multiple calculations after one another in main.

--- main.cpp
+++ main.cpp
@@ -5,28 +5,28 @@ int main()
     std::cout << "SCIENTIFIC CALCULATOR\n\n";
     
     string ans;
-    /*
-    while (ans != "s" && ans != "q")
+   
+    Calculator c;
+    std::cout << "Enter the expression without spaces. Enter q to quit." << std::endl;
+    std::cin >> ans;
+
+    while (ans != "q")
     {
-        if (!ans.empty())
-            std::cout << "Invalid input. ";
-        
-        std::cout << "Press s to start and q to quit.\n";
+        double result = c.computeNew(ans);
+
+        if (result == ((float)(1e+300 * 1e+300)))
+            std::cout << "$ Invalid" << std::endl;
+        else
+            std::cout << "$ " << ans << " = " << result << std::endl;
+
         std::cin >> ans;
     }
 
-    if (ans == "q")
-    {
-        std::cout << "Ending the program...\n";
-        return 0;   // Quit without computation
-    }
-    */
-   
-    Calculator c;
-    //std::cout << "Enter the expression without spaces: ";
-    //std::cin >> ans;
-
-    //std::cout << "$ " << ans << " = " << c.computeNew(ans) << endl;
-
     return 1;   // Quit after computation
 }
+
+/*
+* Still need to addreess:
+* Unmatching parentheses
+* Invalid expression
+*/
