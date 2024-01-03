3de1c28d9ad3285addc9ad14d8e168aa040d7f9e
Sujin Shin <sujins2@illinois.edu>
2024-01-03 21:26:24 +0800

    Added event listeners to buttons (except ans & =) to modify line2

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// declaring variables
+const line1 = document.querySelector("output");
+const line2 = document.getElementById("text");
 const no1 = document.getElementById("no1");
 const no2 = document.getElementById("no2");
 const no3 = document.getElementById("no3");
@@ -8,14 +11,126 @@ const no7 = document.getElementById("no7");
 const no8 = document.getElementById("no8");
 const no9 = document.getElementById("no9");
 const no0 = document.getElementById("no0");
+const dp = document.getElementById("dp");
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
+const mult = document.getElementById("mult");
+const div = document.getElementById("div");
+const pow = document.getElementById("pow");
+const del = document.getElementById("del");
+const ac = document.getElementById("ac");
+const lpar = document.getElementById("lpar");
+const rpar = document.getElementById("rpar");
+const pi = document.getElementById("pi");
+const log = document.getElementById("log");
 
-no1.addEventListener("click", () => {});
-no2.addEventListener("click", () => {});
-no3.addEventListener("click", () => {});
-no4.addEventListener("click", () => {});
-no5.addEventListener("click", () => {});
-no6.addEventListener("click", () => {});
-no7.addEventListener("click", () => {});
-no8.addEventListener("click", () => {});
-no9.addEventListener("click", () => {});
-no0.addEventListener("click", () => {});
+const parCount = {
+  left: 0,
+  right: 0,
+};
+function addInput(a) {
+  line2.value += String(a);
+}
+/* TODO
+ * Move the individual condition to addInput
+ * Add keyboard event listeners
+ * Add ans and = buttons
+ */
+
+// event listeners for buttons
+no1.addEventListener("click", () => {
+  addInput(1);
+});
+no2.addEventListener("click", () => {
+  addInput(2);
+});
+no3.addEventListener("click", () => {
+  addInput(3);
+});
+no4.addEventListener("click", () => {
+  addInput(4);
+});
+no5.addEventListener("click", () => {
+  addInput(5);
+});
+no6.addEventListener("click", () => {
+  addInput(6);
+});
+no7.addEventListener("click", () => {
+  addInput(7);
+});
+no8.addEventListener("click", () => {
+  addInput(8);
+});
+no9.addEventListener("click", () => {
+  addInput(9);
+});
+no0.addEventListener("click", () => {
+  addInput(0);
+});
+dp.addEventListener("click", () => {
+  if (line2.value.at(-1) !== ".") {
+    addInput(".");
+  }
+});
+plus.addEventListener("click", () => {
+  if (line2.value.at(-1) !== "+") {
+    addInput("+");
+  }
+});
+minus.addEventListener("click", () => {
+  if (line2.value.at(-1) !== "-") {
+    addInput("-");
+  }
+});
+mult.addEventListener("click", () => {
+  if (line2.value !== "" && line2.value.at(-1) !== "×") {
+    addInput("×");
+  }
+});
+div.addEventListener("click", () => {
+  if (line2.value !== "" && line2.value.at(-1) !== "÷") {
+    addInput("÷");
+  }
+});
+pow.addEventListener("click", () => {
+  if (line2.value !== "" && line2.value.at(-1) !== "^") {
+    addInput("^");
+  }
+});
+lpar.addEventListener("click", () => {
+  addInput("(");
+  parCount.left += 1;
+});
+rpar.addEventListener("click", () => {
+  if (parCount.left > parCount.right) {
+    addInput(")");
+    parCount.right += 1;
+  }
+});
+pi.addEventListener("click", () => {
+  addInput("π");
+});
+log.addEventListener("click", () => {
+  addInput("log(");
+  parCount.left += 1;
+});
+del.addEventListener("click", () => {
+  if (line2.value.length >= 4 && line2.value.at(-2) === "g") {
+    line2.value = line2.value.slice(0, -4);
+    parCount.left -= 1;
+    return;
+  }
+
+  if (line2.value.at(-1) === "(") {
+    parCount.left -= 1;
+  } else if (line2.value.at(-1) === ")") {
+    parCount.right -= 1;
+  }
+  line2.value = line2.value.slice(0, -1);
+});
+ac.addEventListener("click", () => {
+  line2.value = "";
+  parCount.left = 0;
+  parCount.right = 0;
+});
