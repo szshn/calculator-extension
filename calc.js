f57c3eab7ee0c1115c91006ac604a88e92463d09
Sujin Shin <sujins2@illinois.edu>
2024-01-21 19:51:12 -0600

    toPostfix works for plus and minus for integer values

--- calc.js
+++ calc.js
@@ -1,9 +1,79 @@
 // import stack from "./stack";
+const digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const op1 = ["+", "-"];
+const op2 = ["×", "÷", "^"];
+const allowed = digits.concat(op1, op2);
+
 const toPostfix = (equation) => {
-  return "";
+  let output = [];
+  let op_stack = [];
+
+  for (let i = 0; i < equation.length; i++) {
+    const char = equation[i];
+    if (!allowed.includes(char)) {
+      return [];
+    }
+
+    if (digits.includes(char)) {
+      // is a multi-digit number
+      if (output.length > 0 && i !== 0 && digits.includes(equation[i - 1])) {
+        const new_num = Number(output.pop()) * 10 + Number(char);
+        output.push(new_num);
+      } else {
+        output.push(char);
+      }
+    } else if (char === "(") {
+      op_stack.push(char);
+    } else if (char === ")") {
+      // mismatched parentheses
+      if (op_stack.length === 0) {
+        return [];
+      }
+
+      while (op_stack.at(-1) !== "(") {
+        if (op_stack.length === 0) {
+          return [];
+        }
+
+        output.push(op_stack.at(-1));
+        op_stack.pop();
+      }
+
+      if (op_stack.at(-1) === "(") {
+        op_stack.pop();
+      }
+    }
+
+    // operators
+    else if (op1.includes(char)) {
+      // higher or equal precedence
+      const mayCompute = op2.includes(char)
+        ? op1.concat(op2).includes(op_stack.at(-1))
+        : op1.includes(op_stack.at(-1));
+
+      while (op_stack.length !== 0 && mayCompute) {
+        output.push(op_stack.at(-1));
+        op_stack.pop();
+      }
+      op_stack.push(char);
+    }
+  }
+
+  while (op_stack.length !== 0) {
+    if (op_stack.top !== "(") {
+      output.push(op_stack.at(-1));
+    }
+    op_stack.pop();
+  }
+
+  return output;
 };
 
-export const evaluate = (equation) => {
+const evaluate = (equation) => {
+  console.log(`Computing ${equation}...\n`);
   const postfix = toPostfix(equation);
-  return 23;
+  console.log(`Postfix: ${postfix}`);
+  return postfix.length === 0 ? "Invalid input" : 23;
 };
+
+console.log(`\nResult: ${evaluate("12-3+1")}`);
